231e50f204ee70097177e24c95e0166fb5545fb3
kurasher <27tonokurasher07@gmail.com>
2020-07-13 17:01:41 +0900

    thinking abc133 q2

--- src/abc133/GoodDistance.java
+++ src/abc133/GoodDistance.java
@@ -13,14 +13,21 @@ public class GoodDistance {
 		int D = Integer.parseInt(lines[1]);
 		int[][] X = new int[D][N];
 
-		for(int i = 0; i < N; i++) {
+		for (int i = 0; i < N; i++) {
 			line = sc.nextLine();
 			lines = line.split(" ");
-			for(int j = 0; j < lines.length; j++) {
+			for (int j = 0; j < lines.length; j++) {
 				X[j][i] = Integer.parseInt(lines[j]);
 			}
 		}
 
+		for (int n = 0; n < N; n++) {
+			for (int i = 0; i < D - 1; i++) {
+				for (int j = i + 1; j < D; j++) {
+					Math.abs(X[i][n] - X[j][n]) * Math.abs(X[i][n] - X[j][n]);
+				}
+			}
+		}
 	}
 
 }
