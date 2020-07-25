c7ff198a75681d738b1874d6ab57e0225735c7fb
kurasher <27tonokurasher07@gmail.com>
2020-07-25 12:41:38 +0900

    thinking abc125 q2

---
+++ src/abc125/Resale.java
@@ -0,0 +1,18 @@
+package abc125;
+
+import java.util.Scanner;
+
+public class Resale {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+
+		int[] V = new int[N];
+		int[] C = new int[N];
+
+
+	}
+
+}
