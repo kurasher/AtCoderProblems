d350b36cf07dc8d15da7e268cc8c055ac9ce1b85
kurasher <27tonokurasher07@gmail.com>
2020-07-25 12:56:51 +0900

    add abc125 w2

--- src/abc125/Resale.java
+++ src/abc125/Resale.java
@@ -8,10 +8,33 @@ public class Resale {
 		// TODO 自動生成されたメソッド・スタブ
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-
 		int[] V = new int[N];
 		int[] C = new int[N];
 
+		for(int i = 0; i < N; i++) {
+			V[i] = sc.nextInt();
+		}
+		for(int i = 0; i < N; i++) {
+			C[i] = sc.nextInt();
+		}
+
+		int Vsum = 0;
+		int Csum = 0;
+		int max = Vsum - Csum;
+		for(int i = 0; i < N; i++) {
+			Vsum += V[i];
+			Csum += C[i];
+
+			if(max < (Vsum - Csum)) {
+				max = Vsum - Csum;
+			}else {
+				Vsum -= V[i];
+				Csum -= C[i];
+			}
+
+		}
+
+		System.out.println(max);
 
 	}
 
