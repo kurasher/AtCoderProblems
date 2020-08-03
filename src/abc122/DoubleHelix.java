da93eff13ed2fb747f8aa57c10dd7c4086e326f0
kurasher <27tonokurasher07@gmail.com>
2020-08-03 22:25:51 +0900

    add abc122 q2

--- src/abc122/DoubleHelix.java
+++ src/abc122/DoubleHelix.java
@@ -1,10 +1,23 @@
 package abc122;
 
+import java.util.Scanner;
+
 public class DoubleHelix {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		Scanner sc = new Scanner(System.in);
+		String line = sc.nextLine();
+		
+		if(line.equals("A")) {
+			System.out.println("T");
+		}else if(line.equals("T")) {
+			System.out.println("A");			
+		}else if(line.equals("C")) {
+			System.out.println("G");			
+		}else {
+			System.out.println("C");			
+		}
 	}
 
 }
