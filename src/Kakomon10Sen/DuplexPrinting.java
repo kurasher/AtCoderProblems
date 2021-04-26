06c63edeb949581615e27458fd7425e6b784d517
kurasher <27tonokurasher07@gmail.com>
2021-04-26 23:01:41 +0900

    [add] abc157 q1

---
+++ src/Kakomon10Sen/DuplexPrinting.java
@@ -0,0 +1,12 @@
+package Kakomon10Sen;
+
+import java.util.Scanner;
+
+public class DuplexPrinting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        System.out.println((N+2-1)/2);
+    }
+}
